b9efe0c80fa7f7c7b2bfdfb7e4964bcdafa0e444
circunferenzoide <miligiudice@gmail.com>
2020-10-15 15:16:51 -0300

    cambios xml
    cambie xml y agregue en experto las cosas para llenar la bd

--- src/Main/MesaDeAyuda.java
+++ src/Main/MesaDeAyuda.java
@@ -9,9 +9,9 @@ import java.util.List;
 public class MesaDeAyuda {
 
     public static void main(String[] args) {
-       MainMenu menu = new MainMenu();
-       menu.setVisible(true);
-       // Controlador().Metodo();
+       //MainMenu menu = new MainMenu();
+       //menu.setVisible(true);
+       new Controlador().Metodo();
        //ControladorABMTipoInstancia control = new ControladorABMTipoInstancia();
        //List<DTOTipoInstancia> lista = control.buscarTipoInstancias();
        //System.out.println("break");
